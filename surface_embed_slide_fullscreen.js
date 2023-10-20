18b535a8343e46991c69daf46957486b80c9c712
Saharsh Agrawal <saharsh@withsurface.com>
2023-10-20 16:30:29 -0700

    Update surface_embed_slide_fullscreen.js

--- surface_embed_slide_fullscreen.js
+++ surface_embed_slide_fullscreen.js
@@ -53,7 +53,7 @@ function embedSurfaceForm() {
           left: 0;
           width: 100%;
           height: 100%;
-          z-index: 1000;
+          z-index: 99999;
           background-color: rgba(0,0,0,0.5);
           opacity: 0;
           transition: opacity 0.3s ease;
